38daa65bd4a95301a566c7e8f8b32fc90f341954
Michiel Muller <r0840937@student.thomasmore.be>
2022-04-19 23:38:50 +0200

    require

--- assests/js/buiapi.js
+++ assests/js/buiapi.js
@@ -1,3 +1,5 @@
+const https = require('https');
+
 function updateDataBuienradar(){
 	let req = https.get("https://api.openweathermap.org/data/2.5/weather?lat=51.319978006056466&lon=4.868497528758273&appid=a0fe2db2e4fd8ca09f4d160476a72330&units=metric", function(res) {
 		let data = '', json_data;
