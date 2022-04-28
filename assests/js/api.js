c56b6f4ee9802f3a2a04c23bac22db56cc74b40f
Michiel Muller <r0840937@student.thomasmore.be>
2022-04-28 10:15:28 +0200

    change

--- assests/js/api.js
+++ assests/js/api.js
@@ -1,8 +1,56 @@
-const userAction = async () => {
-    const response = await fetch('https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/', {
-      mode: 'no-cors'
-    });
-    const myJson = await response.json(); //extract JSON from the http response
-    // do something with myJson
-    return myJson;
-  }
+// const userAction = async () => {
+//     const response = await fetch('https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/', {
+//       mode: 'no-cors'
+//     });
+//     const myJson = await response.json(); //extract JSON from the http response
+//     // do something with myJson
+//     return myJson;
+//   }
+
+// api url
+const api_url = 
+      "https://employeedetails.free.beeceptor.com/my/api/path";
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+  
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = 
+        `<tr>
+          <th>Name</th>
+          <th>Office</th>
+          <th>Position</th>
+          <th>Salary</th>
+         </tr>`;
+    
+    // Loop to access all rows 
+    for (let r of data.list) {
+        tab += `<tr> 
+    <td>${r.name} </td>
+    <td>${r.office}</td>
+    <td>${r.position}</td> 
+    <td>${r.salary}</td>          
+</tr>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("employees").innerHTML = tab;
+}
