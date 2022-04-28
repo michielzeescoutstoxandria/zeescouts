0dd1225282b6d553f191f7bbdb66c0b826c49cfe
Michiel Muller <r0840937@student.thomasmore.be>
2022-04-28 10:10:39 +0200

    fix error

--- assests/js/api.js
+++ assests/js/api.js
@@ -2,7 +2,7 @@ const userAction = async () => {
     const response = await fetch('https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/', {
       mode: 'no-cors'
     });
-    // const myJson = response.json(); //extract JSON from the http response
+    const myJson = response.json(); //extract JSON from the http response
     // do something with myJson
-    // console.log(myJson)
+    console.log(myJson);
   }
