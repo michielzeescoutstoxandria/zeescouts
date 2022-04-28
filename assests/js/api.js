651633a1a81c66ac204fd71e060528386266ccb2
Michiel Muller <r0840937@student.thomasmore.be>
2022-04-28 09:18:36 +0200

    test

--- assests/js/api.js
+++ assests/js/api.js
@@ -1,10 +1,5 @@
 const userAction = async () => {
-    const response = await fetch('https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+    const response = await fetch('https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/');
     const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
     console.log(myJson)
