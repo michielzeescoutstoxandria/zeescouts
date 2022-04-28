d8b642fbdfc28eb7be11b0a65929cb35661d5d8e
Michiel Muller <r0840937@student.thomasmore.be>
2022-04-28 12:14:56 +0200

    tes api

--- assests/js/api.js
+++ assests/js/api.js
@@ -15,7 +15,7 @@ function initKeycloak() {
   keycloak.init({onLoad: 'login-required'}).then(function() {
       // constructTableRows(keycloak.idTokenParsed);
       // pasteToken(keycloak.token);
-      getapi(api_url);
+      // getapi(api_url);
   }).catch(function() {
       alert('failed to initialize');
   });
