3eed313cad64d098f6984c59e19ae0242e61b8bc
Michiel Muller <r0840937@student.thomasmore.be>
2022-04-28 09:24:46 +0200

    no-cors

--- assests/js/api.js
+++ assests/js/api.js
@@ -1,6 +1,6 @@
 const userAction = async () => {
     const response = await fetch('https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/', {
-      mode: 'cors',
+      mode: 'no-cors',
     });
     const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
