66e7fcbb8be194c4ceb1bd3cdc270095be126e80
Michiel Muller <r0840937@student.thomasmore.be>
2022-04-28 21:46:34 +0200

    add json function

--- assests/js/api.js
+++ assests/js/api.js
@@ -1,20 +1,7 @@
-// const userAction = async () => {
-//     const response = await fetch('https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/rest-ga/', {
-//       mode: 'no-cors'
-//     });
-//     const myJson = await response.json(); //extract JSON from the http response
-//     // do something with myJson
-//     return myJson;
-//   }
-
-// api url
-
 var keycloak = new Keycloak();
 
 function initKeycloak() {
   keycloak.init({onLoad: 'login-required'}).then(function() {
-      // constructTableRows(keycloak.idTokenParsed);
-      // pasteToken(keycloak.token);
       // getapi(api_url, keycloak.token);
       loadData();
       // console.log(keycloak.token);
@@ -58,28 +45,6 @@ async function getapi(url, token) {
 function hideloader() {
     document.getElementById('loading').style.display = 'none';
 }
-// Function to define innerHTML for HTML table
-function show(data) {
-    let tab = 
-        `<tr>
-          <th>Name</th>
-          <th>Office</th>
-          <th>Position</th>
-          <th>Salary</th>
-         </tr>`;
-    
-    // Loop to access all rows 
-    for (let r of data.list) {
-        tab += `<tr> 
-    <td>${r.name} </td>
-    <td>${r.office}</td>
-    <td>${r.position}</td> 
-    <td>${r.salary}</td>          
-</tr>`;
-    }
-    // Setting innerHTML as tab variable
-    document.getElementById("employees").innerHTML = tab;
-}
 
 const loadData = function () {
   // document.getElementById('username').innerText = keycloak.subject;
@@ -94,11 +59,8 @@ const loadData = function () {
   req.onreadystatechange = function () {
       if (req.readyState == 4) {
           if (req.status == 200) {
-              alert('Success');
-              
-              var myArr = JSON.parse(this.responseText);
-              document.getElementById("employees").innerHTML = myArr;
-              console.log(myArr.functies);
+            var myArr = JSON.parse(this.responseText);
+            json(myArr);
               
           } else if (req.status == 403) {
               alert('Forbidden');
@@ -107,6 +69,9 @@ const loadData = function () {
   }
 
   req.send();
-  
-  
+}
+
+function json(json) {
+  var functie = json.functie;
+  console.log(functie);
 }
