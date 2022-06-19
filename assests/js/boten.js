55e5291fecd5a79830bfe8511744a0f6384ec555
Michiel Muller <r0840937@student.thomasmore.be>
2022-06-19 22:48:38 +0200

    no cors

--- assests/js/boten.js
+++ assests/js/boten.js
@@ -4,6 +4,7 @@ function boten() {
   
     const req = new XMLHttpRequest();
     req.open('GET', url, true);
+    req.setRequestHeader('mode', 'no-cors');
     console.log(req);
   
     req.onreadystatechange = function () {
