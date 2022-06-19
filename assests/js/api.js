718efd3aacea11365eafdcfb29a3a3542e2a3ca3
Michiel Muller <r0840937@student.thomasmore.be>
2022-06-19 12:03:10 +0200

    change script boten

--- assests/js/api.js
+++ assests/js/api.js
@@ -132,24 +132,3 @@ function redirect(){
 //     // }
 //   // }  
 // }
-
-function boten() {
-  const url = 'https://zeescouts.000webhostapp.com/getboten.php';
-
-  const req = new XMLHttpRequest();
-  req.open('GET', url, true);
-  req.setRequestHeader('Accept', 'application/json');
-  req.setRequestHeader('mode', 'no-cors');
-
-  req.onreadystatechange = function () {
-      if (req.readyState == 4) {
-          if (req.status == 200) {
-            var json = JSON.parse(this.responseText);
-            console.log(json);
-             
-          } else if (req.status == 403) {
-              alert('Forbidden');
-          }
-      }
-  }
-}
