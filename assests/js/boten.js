e39ff57788a96db69836046aa13cb5e9c07a2942
Michiel Muller <r0840937@student.thomasmore.be>
2022-08-16 00:33:55 +0200

    test

--- assests/js/boten.js
+++ assests/js/boten.js
@@ -1,21 +1,58 @@
 function boten() {
     console.log('start');
     const url = 'https://zeescouts.000webhostapp.com/getboten.php';
-  
+
     const req = new XMLHttpRequest();
     req.open('GET', url, true);
     console.log(req);
-  
+
     req.onreadystatechange = function () {
         if (req.readyState == 4) {
             if (req.status == 200) {
-              var json = JSON.parse(this.responseText);
-              console.log(json);
-               
+                var json = JSON.parse(this.responseText);
+                console.log(json);
+
             } else if (req.status == 403) {
                 alert('Forbidden');
             }
         }
     }
     req.send();
-  }
+}
+
+function loadboten() {
+    console.log('start');
+    const url = 'https://zeescouts.000webhostapp.com/getboten.php';
+
+    const req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    console.log(req);
+
+    req.onreadystatechange = function () {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                var json = JSON.parse(this.responseText);
+                console.log(json);
+                var boten = json.boten;
+                var botenlijst = document.getElementById('botenlijst');
+                for (var i = 0; i < boten.length; i++) {
+                    var botenitem = document.createElement('li');
+                    botenitem.innerHTML = boten[i].naam;
+                    botenlijst.appendChild(botenitem);
+                }
+            } else if (req.status == 403) {
+                alert('Forbidden');
+            }
+        }
+    }
+    req.send();
+}
+
+function loadcards() { 
+    loadboten();
+    for (var i = 0; i < boten.length; i++) {
+        var botenitem = document.createElement('li');
+        botenitem.innerHTML = boten[i].naam;
+        botenlijst.appendChild(botenitem);
+    }
+}
