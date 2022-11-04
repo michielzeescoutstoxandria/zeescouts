560fec9c04f620620ca3eff96f093504d5ead378
Michiel Muller <r0840937@student.thomasmore.be>
2022-11-04 23:12:34 +0100

    change api url

--- assets/js/boten.js
+++ assets/js/boten.js
@@ -23,7 +23,7 @@
 
 function loadboten() {
     console.log('start');
-    const url = 'https://mullmull.epizy.com/get.php';
+    const url = 'https://mullmull.epizy.com/getboten.php';
 
     const req = new XMLHttpRequest();
     req.open('GET', url, true);
