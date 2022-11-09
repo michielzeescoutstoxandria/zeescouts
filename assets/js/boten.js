2751452e3e1779bbd54ff74edeb3c5e9f78fee70
Michiel Muller <r0840937@student.thomasmore.be>
2022-11-09 23:22:29 +0100

    add colorcode

--- assets/js/boten.js
+++ assets/js/boten.js
@@ -38,15 +38,45 @@ function loadboten() {
                 // var botenitem = document.createElement('li');
                 // botenitem.innerHTML = boten[i].naam;
                 // botenlijst.appendChild(botenitem);
-                var botenitem = '<div class="card"><img src="https://drive.google.com/uc?export=view&id=' + boten[i].img_ID + '" class="card-img-top" width="20px"><div class="card-body clearfix"><h4 class="card-title">' + boten[i].naam + '</h4><p class="card-text border-light border-top border-bottom"><span class="row"><span class="col-6 text-muted text-left"><a href="' + boten[i].datasheet + '" class="btn btn-outline-dark float-left">Datasheet</a></span><span class="col-6 text-muted text-right"><a href="javascript: void(0);" class="btn btn-outline-dark float-right schade" onclick="schadeBtn(this)" data-boot=' + boten[i].id + ' data-bootnaam="' + boten[i].naam + '">schade</a></span></span></p><p class="card-text">' + boten[i].description + '</p></div></div>';
+                var botenitem = `
+                    <div class="card"><img src="https://drive.google.com/uc?export=view&id=${boten[i].img_ID}" class="card-img-top"
+                            width="20px">
+                        <div class="card-body clearfix">
+                            <h4 class="card-title">${boten[i].naam}</h4>
+                            <div class="kleurdiv" id="kleurdiv${i}">
+                                
+                            </div>
+                            <p class="card-text border-light border-top border-bottom">
+                                <span class="row">
+                                    <span class="col-6 text-muted text-left">
+                                        <a href="${boten[i].datasheet}" class="btn btn-outline-dark float-left">Datasheet</a>
+                                    </span>
+                                    <span class="col-6 text-muted text-right">
+                                        <a href="javascript: void(0);" class="btn btn-outline-dark float-right schade" onclick="schadeBtn(this)"
+                                            data-boot=${boten[i].id} data-bootnaam="${boten[i].naam}">schade</a>
+                                    </span>
+                                </span>
+                            </p>
+                            <p class="card-text">${boten[i].description}</p>
+                        </div>
+                    </div>
+                `;
                 botenlijst.innerHTML += botenitem;
 
                 select = document.getElementById('bootnaam');
                 var opt = document.createElement('option');
-                opt.setAttribute('data-bootid',  boten[i].id);
+                opt.setAttribute('data-bootid', boten[i].id);
                 opt.value = boten[i].naam;
                 opt.innerHTML = boten[i].naam;
                 select.appendChild(opt);
+
+                if(boten[i].color_1 !== null){
+                    const kleurdiv = document.getElementById(`kleurdiv${i}`);
+                    const kleuren = `
+                        <span class="kleur1" style="background-color:#${boten[i].color_1}"></span>
+                        <span class="kleur2" style="background-color:#${boten[i].color_2}"></span>`;
+                    kleurdiv.innerHTML += kleuren;
+                }
 
             }
 
@@ -65,19 +95,38 @@ function loadcards() {
     //     botenlijst.appendChild(botenitem);
     // }
     // var botenlijst = document.getElementById('boot');
-    // var botenitem = '<div class="card"><img src="https://drive.google.com/uc?export=view&id=1-YfVH0eq2lwVyU3nWc9dOh_N9e1F2LXJ" class="card-img-top" width="20px"><div class="card-body clearfix"><h4 class="card-title">stormvogel</h4><p class="card-text border-light border-top border-bottom"><span class="row"><span class="col-6 text-muted text-left"><a href="" class="btn btn-outline-dark float-left">Datasheet</a></span><span class="col-6 text-muted text-right"><a href="javascript: void(0);" class="btn btn-outline-dark float-right schade"  onclick="schadeBtn(this)" data-boot=0>schade</a></span></span></p><p class="card-text">demo model</p></div></div>';
+    // var botenitem = `
+    //                 <div class="card"><img src="https://drive.google.com/uc?export=view&id=1-YfVH0eq2lwVyU3nWc9dOh_N9e1F2LXJ" class="card-img-top"
+    //                         width="20px">
+    //                     <div class="card-body clearfix">
+    //                         <h4 class="card-title">stormvogel</h4>
+    //                         <div class="kleurdiv">
+    //                             <span class="kleur1"></span>
+    //                             <span class="kleur2"></span>
+    //                         </div>
+    //                         <p class="card-text border-light border-top border-bottom">
+    //                             <span class="row">
+    //                                 <span class="col-6 text-muted text-left">
+    //                                     <a href="" class="btn btn-outline-dark float-left">Datasheet</a>
+    //                                 </span>
+    //                                 <span class="col-6 text-muted text-right">
+    //                                     <a href="javascript: void(0);" class="btn btn-outline-dark float-right schade" onclick="schadeBtn(this)"
+    //                                         data-boot=stormvogel data-bootnaam="1">schade</a>
+    //                                 </span>
+    //                             </span>
+    //                         </p>
+    //                         <p class="card-text">Dit is een test card</p>
+    //                     </div>
+    //                 </div>
+    //             `;
     // botenlijst.innerHTML += botenitem;
-    // botenitem = '<div class="card"><img src="https://drive.google.com/uc?export=view&id=1-YfVH0eq2lwVyU3nWc9dOh_N9e1F2LXJ" class="card-img-top" width="20px"><div class="card-body clearfix"><h4 class="card-title">stormvogel</h4><p class="card-text border-light border-top border-bottom"><span class="row"><span class="col-6 text-muted text-left"><a href="" class="btn btn-outline-dark float-left">Datasheet</a></span><span class="col-6 text-muted text-right"><a href="javascript: void(0);" class="btn btn-outline-dark float-right schade"  onclick="schadeBtn(this)" data-boot=1>schade</a></span></span></p><p class="card-text">demo model</p></div></div>';
-    // botenlijst.innerHTML += botenitem;
-    // var botenitem = '<div class="card"><img src="https://drive.google.com/uc?export=view&id=1-YfVH0eq2lwVyU3nWc9dOh_N9e1F2LXJ" class="card-img-top" width="20px"><div class="card-body clearfix"><h4 class="card-title">stormvogel</h4><p class="card-text border-light border-top border-bottom"><span class="row"><span class="col-6 text-muted text-left"><a href="" class="btn btn-outline-dark float-left">Datasheet</a></span><span class="col-6 text-muted text-right"><a href="javascript: void(0);" class="btn btn-outline-dark float-right schade"  onclick="schadeBtn(this)" data-boot=2>schade</a></span></span></p><p class="card-text">demo model</p></div></div>';
-    // botenlijst.innerHTML += botenitem;
-    // var botenitem = '<div class="card"><img src="https://drive.google.com/uc?export=view&id=1-YfVH0eq2lwVyU3nWc9dOh_N9e1F2LXJ" class="card-img-top" width="20px"><div class="card-body clearfix"><h4 class="card-title">stormvogel</h4><p class="card-text border-light border-top border-bottom"><span class="row"><span class="col-6 text-muted text-left"><a href="" class="btn btn-outline-dark float-left">Datasheet</a></span><span class="col-6 text-muted text-right"><a href="javascript: void(0);" class="btn btn-outline-dark float-right schade"  onclick="schadeBtn(this)" data-boot=3>schade</a></span></span></p><p class="card-text">demo model</p></div></div>';
-    // botenlijst.innerHTML += botenitem;
-    // botenlijst.innerHTML += botenitem;
-    // botenlijst.innerHTML += botenitem;
-    // botenlijst.innerHTML += botenitem;
-    // botenlijst.innerHTML += botenitem;
-    // botenlijst.innerHTML += botenitem;
+
+    // select = document.getElementById('bootnaam');
+    // var opt = document.createElement('option');
+    // opt.setAttribute('data-bootid', '1');
+    // opt.value = 'stormvogel';
+    // opt.innerHTML = 'stormvogel';
+    // select.appendChild(opt);
 
 }
 
