f90e819ae25709c91c0b52f936818ce70150ccab
michiel muller <michiel@zeescoutstoxandria.be>
2023-03-14 16:25:34 +0100

    test

--- keycloak.js
+++ keycloak.js
@@ -2,42 +2,58 @@
 // let keycloak = new Keycloak({});
 
 
-var session = require('express-session');
-var Keycloak = require('keycloak-connect');
+// var session = require('express-session');
+// var Keycloak = require('keycloak-connect');
 
-let _keycloak;
+// let _keycloak;
 
-var keycloakConfig = {
-    clientId: 'nodejs-microservice',
-    bearerOnly: true,
-    serverUrl: 'http://localhost:8080/auth',
-    realm: 'Demo-Realm',
-    credentials: {
-        secret: '62c99f7c-da55-48fb-ae4e-a27f132546b7'
-    }
+// var keycloakConfig = {
+//     clientId: 'nodejs-microservice',
+//     bearerOnly: true,
+//     serverUrl: 'http://localhost:8080/auth',
+//     realm: 'Demo-Realm',
+//     credentials: {
+//         secret: '62c99f7c-da55-48fb-ae4e-a27f132546b7'
+//     }
+// };
+
+// function initKeycloak() {
+//     if (_keycloak) {
+//         console.warn("Trying to init Keycloak again!");
+//         return _keycloak;
+//     } 
+//     else {
+//         console.log("Initializing Keycloak...");
+//         var memoryStore = new session.MemoryStore();
+//         _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+//         return _keycloak;
+//     }
+// }
+
+// function getKeycloak() {
+//     if (!_keycloak){
+//         console.error('Keycloak has not been initialized. Please called init first.');
+//     } 
+//     return _keycloak;
+// }
+
+// module.exports = {
+//     initKeycloak,
+//     getKeycloak
+// };
+
+import Keycloak from 'keycloak-js';
+import config from './keycloak.json';
+
+const keycloak = Keycloak(config);
+
+export const initKeycloak = () => {
+  return keycloak.init({
+    onLoad: 'check-sso',
+    silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html'
+  });
 };
 
-function initKeycloak() {
-    if (_keycloak) {
-        console.warn("Trying to init Keycloak again!");
-        return _keycloak;
-    } 
-    else {
-        console.log("Initializing Keycloak...");
-        var memoryStore = new session.MemoryStore();
-        _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
-        return _keycloak;
-    }
-}
-
-function getKeycloak() {
-    if (!_keycloak){
-        console.error('Keycloak has not been initialized. Please called init first.');
-    } 
-    return _keycloak;
-}
-
-module.exports = {
-    initKeycloak,
-    getKeycloak
+export const isLoggedIn = () => {
+  return keycloak.authenticated;
 };
