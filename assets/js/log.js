db2509bd6a0c72a2dbd70ed3c799c48df52051d6
michiel muller <michiel@zeescoutstoxandria.be>
2023-03-16 11:00:45 +0100

    test

--- assets/js/log.js
+++ assets/js/log.js
@@ -15,7 +15,59 @@ function loadschade() {
             for (var i = 0; i < schade.length; i++) {
                 var schadeitem = `
                     <tr>
-                        <td>${schade[i].id}</td>
+                        <td>${schade[i].bootnaam}</td>
+                        <td>${schade[i].prioriteit}</td>
+                        <td>${schade[i].description}</td>
+                        <td>${schade[i].persoon}</td>
+                        <td>
+                                    
+                                    <label class="switch">
+                                        
+                                        <input type="checkbox" id="check${i}" data-id="${schade[i].id}" onclick="voltooid(this)" ${checkvoltooid(schade[i].voltooid)}>
+                                        <span class="slider round"></span>
+                                        
+                                    </label>
+                        </td>
+                        
+                    </tr>
+                `;
+                if (schade[i].voltooid == 0){
+                    schadelijst.innerHTML += schadeitem;
+                }
+            }
+
+        })
+        .catch(function (err) {
+            console.log("Something went wrong!", err);
+            alert("tis kapot");
+        });
+}
+
+function checkvoltooid(voltooid) {
+    console.log(voltooid);
+    if (voltooid == 1) {
+        return "checked";
+    } else {
+        return "";
+    }
+}
+
+function loadlog() {
+    fetch('https://apizee1.000webhostapp.com/getschade.php', {
+        headers: {
+            'Accept': 'application/json',
+        },
+        method: "GET", // POST, PUT, DELETE, etc.
+        mode: "cors", // same-origin, no-cors
+    })
+        .then(response => response.json())
+        .then(function (data) {
+            console.log(data);
+            var schade = data;
+            var schadelijst = document.getElementById('table-schade');
+            for (var i = 0; i < schade.length; i++) {
+                var schadeitem = `
+                    <tr>
                         <td>${schade[i].bootnaam}</td>
                         <td>${schade[i].prioriteit}</td>
                         <td>${schade[i].description}</td>
@@ -33,7 +85,6 @@ function loadschade() {
                     </tr>
                 `;
                 schadelijst.innerHTML += schadeitem;
-
             }
 
         })
@@ -41,15 +92,6 @@ function loadschade() {
             console.log("Something went wrong!", err);
             alert("tis kapot");
         });
-}
-
-function checkvoltooid(voltooid){
-    console.log(voltooid);
-    if(voltooid == 1){
-        return "checked";
-    } else {
-        return "";
-    }
 }
 
 function loadkapot() {
@@ -68,7 +110,6 @@ function loadkapot() {
             for (var i = 0; i < boot.length; i++) {
                 var bootitem = `
                     <tr>
-                        <td>${boot[i].id}</td>
                         <td>${boot[i].naam}</td>
                         <td>
                                     
@@ -97,7 +138,7 @@ function loadtable() {
     loadschade();
 }
 
-function loadboten(){
+function loadboten() {
     loadkapot();
 }
 
@@ -108,33 +149,35 @@ function voltooid(d) {
     var bool
     data.append("id", id);
     data.append("schade_id", schade_id);
-    if(d.checked == true){
+    if (d.checked == true) {
         bool = 1
-    }else{
+    } else {
         bool = 0
     }
     data.append("voltooid", bool);
 
     // (B) INIT FETCH POST
     fetch("https://apizee1.000webhostapp.com/postvoltooid.php", {
-      method: "POST",
-      body: data
+        method: "POST",
+        body: data
     })
 
-      // (C) RETURN SERVER RESPONSE AS TEXT
-      .then((result) => {
-        if (result.status != 200) { throw new Error("Bad Server Response"); }
-        return result.text();
-      })
+        // (C) RETURN SERVER RESPONSE AS TEXT
+        .then((result) => {
+            if (result.status != 200) { throw new Error("Bad Server Response"); }
+            return result.text();
+        })
 
-      // (D) SERVER RESPONSE
-      .then((response) => {
-        console.log(response);
-      })
+        // (D) SERVER RESPONSE
+        .then((response) => {
+            console.log(response);
+        })
 
-      // (E) HANDLE ERRORS - OPTIONAL
-      .catch((error) => { console.log(error); 
-    alert("tis kapot")});
+        // (E) HANDLE ERRORS - OPTIONAL
+        .catch((error) => {
+            console.log(error);
+            alert("tis kapot")
+        });
 
     // (F) PREVENT FORM SUBMIT
     return false;
@@ -147,33 +190,35 @@ function beschikbaar(d) {
     var bool
     data.append("id", id);
     data.append("boot_id", boot_id);
-    if(d.checked == true){
+    if (d.checked == true) {
         bool = 1
-    }else{
+    } else {
         bool = 0
     }
     data.append("beschikbaar", bool);
 
     // (B) INIT FETCH POST
     fetch("https://apizee1.000webhostapp.com/postbeschikbaar.php", {
-      method: "POST",
-      body: data
+        method: "POST",
+        body: data
     })
 
-      // (C) RETURN SERVER RESPONSE AS TEXT
-      .then((result) => {
-        if (result.status != 200) { throw new Error("Bad Server Response"); }
-        return result.text();
-      })
+        // (C) RETURN SERVER RESPONSE AS TEXT
+        .then((result) => {
+            if (result.status != 200) { throw new Error("Bad Server Response"); }
+            return result.text();
+        })
 
-      // (D) SERVER RESPONSE
-      .then((response) => {
-        console.log(response);
-      })
+        // (D) SERVER RESPONSE
+        .then((response) => {
+            console.log(response);
+        })
 
-      // (E) HANDLE ERRORS - OPTIONAL
-      .catch((error) => { console.log(error); 
-    alert("tis kapot")});
+        // (E) HANDLE ERRORS - OPTIONAL
+        .catch((error) => {
+            console.log(error);
+            alert("tis kapot")
+        });
 
     // (F) PREVENT FORM SUBMIT
     return false;
